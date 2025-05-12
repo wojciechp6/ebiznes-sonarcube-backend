aa0593c2704a4eab309599daf4dfdf120a801cae
wpalka-icg <wpalka@icecodegames.com>
2025-05-12 23:41:13 +0200

    backend only

--- frontend/frontend/src/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api",
-    headers: { "Content-Type": "application/json" }
-});
-
-export default api;
